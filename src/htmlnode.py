62e094d29d905c1271af39bcc668b56bc068f830
christopherhanke <christopherhanke@googlemail.com>
2024-04-17 21:51:22 +0200

    added LeafNode(HTMLNode) and unittests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -16,3 +16,19 @@ class HTMLNode:
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None):
+        if value == None:
+            raise ValueError()
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.tag == None:
+            return self.value
+        new_string = "<" + self.tag
+        if self.props != None:
+            new_string += self.props_to_html()
+        new_string += ">" + self.value + "</" + self.tag + ">"
+        return new_string
+        
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,7 @@
 import unittest
 
 from htmlnode import HTMLNode
+from htmlnode import LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     node = HTMLNode()
@@ -19,6 +20,24 @@ class TestHTMLNode(unittest.TestCase):
             self.node.to_html()
         except NotImplementedError:
             pass
+
+class TestLeafNode(unittest.TestCase):
+    leaf1 = LeafNode("p", "This is a paragraph of text.")
+    leaf1_string = "<p>This is a paragraph of text.</p>"
+    leaf2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+    leaf2_string = '<a href="https://www.google.com">Click me!</a>'
+
+    def test_empty_node(self):
+        try:
+            leaf_empty = LeafNode()
+        except ValueError:
+            pass
+    
+    def test_to_html_1(self):
+        self.assertEqual(self.leaf1.to_html(), self.leaf1_string)
+    
+    def test_to_html_2(self):
+        self.assertEqual(self.leaf2.to_html(), self.leaf2_string)
 
 if __name__ == "__main__":
     unittest.main()
